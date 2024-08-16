c8f23e2c93aef5d05b3d9ad943c9a95def728685
M-Umar-Hafeez <umarhafeez049@gmail.com>
2024-08-16 21:33:12 +0500

    Add files via upload

---
+++ Java script assignment 1/app.js
@@ -0,0 +1,135 @@
+// "1)"
+alert ("Welcome to Our Website")
+// "2)"
+alert ("Error! Please Enter a valid password")
+// "3)"
+alert ("Hello \n person")
+// "4)"
+alert ("This is my new Project")
+alert ("Made with javascript")
+alert ("using alert")
+// "5)"
+alert ("This is my new project")
+// "6)"
+alert ("I dont know how to do this :(")
+// "7)"
+var Username;
+// "8)"
+var Myname = "Umar Hafeez"
+console.log(Myname)
+// "9)"
+var message = "Hello World"
+alert (message)
+// "10)"
+var Name =prompt("name")
+var clas =prompt("class")
+var section =prompt("section")
+var biodat = Name + clas + section
+alert (biodat)
+// "11)"
+var email = "umarhafeez049@gmail.com"
+alert (email)
+// "12,13)"
+var decorative_text = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert (decorative_text)
+// "14)"
+var age = "Age=15"
+alert (age)
+// "15)"
+alert ("dont know either :(")
+// "16)"
+var birthyear = "2009 A.D"
+alert (birthyear)
+// "17)"
+var visitorName="Someone"
+var product= "Jeans"
+var quantity = 7  
+console.log(visitorName + "ordered " + quantity + " " +product+ "on XYZ Clothing Store")
+// "18)"
+var a = 10, b=20, c=30
+// "19)"
+//Legal Variables Names
+// var userAge;
+// var totalPrice;
+// var isActive;
+// var maxValue;
+// var dataList;
+//ILLEGAL Variables Name
+// var 'userAge';
+// var 1totalPrice;
+// var first name;
+// var class;
+// var @userAge;
+// "20)"
+//a)
+document.write("<h1>Rules for naming JS variable </h1>")
+//b)
+document.write("<br>Variable names can only contain Digits, Letters,Underscore and Dollar signs. For example $my_1stVariable ")
+//c)
+document.write("<br>Variables must begin with a Digits,Letters or Dollar signs. For example$name, _name or name")
+//d)
+document.write("<br>Variable names are case Sensitive")
+//e)
+document.write("<br>Variable names should not be JS Reserved Words")
+
+//"21)"
+var num1=+prompt("Enter First Number for additon")
+var num2 = +prompt("Enter Second Number for addition")
+var result = console.log(num1 + num2)
+//"22)"
+var num3=+prompt("Enter First Number for multiplication")
+var num4 = +prompt("Enter Second Number for multiplication")
+var result1 = console.log(num1*num2)
+
+var num5=+prompt("Enter First Number for subtraction")
+var num6 =+prompt("Enter Second Number for subtraction")
+var result2 = console.log(num1-num2)
+
+var num7=+prompt("Enter First Number for division")
+var num8 =+prompt("Enter Second Number for division")
+var result3 = console.log(num1/num2)
+console.log("I dont Know how to perform Modulus calculation.")
+//"23)"
+//a) 
+var a;
+//b)
+a=5
+console.log(a) // variable after declaration
+//c)
+a=5;
+//d)
+console.log(a)
+//e)
+a++
+//f)
+console.log(a)
+//g)
+var b= a+7;
+//h)
+console.log(b)
+//i)
+b--
+//j)
+console.log(b)
+//k)
+var c = b/3;
+//l)
+console.log(c)
+//"24)"
+var ticket = 600
+var price_of_five = 600*5
+console.log(price_of_five)
+//"25)"
+document.write("<br><br><br><h1>Table of 5</h1>")
+var a = 5
+
+document.write("<br>5x2 =" + a*2)
+document.write("<br>5x1 =" + a*1)
+document.write("<br>5x3 =" + a*3)
+document.write("<br>5x4 =" + a*4)
+document.write("<br>5x5 =" + a*5)
+document.write("<br>5x6 =" + a*6)
+document.write("<br>5x7 =" + a*7)
+document.write("<br>5x8 =" + a*8)
+document.write("<br>5x9 =" + a*9)
+document.write("<br>5x10=" + a*10)
